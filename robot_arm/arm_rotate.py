b2bc69c4c1a2efc9f054bb3924da043a3adff949
破竹 <2986779260@qq.com>
2025-04-03 21:51:25 +0800

    fix json bug

--- robot_arm/arm_rotate.py
+++ robot_arm/arm_rotate.py
@@ -8,7 +8,7 @@ Arm = Arm_Device()
 time.sleep(.1)
 
 def arm_rotate(angle):
-    Arm.Arm_serial_servo_write6(1, angle, 1000)
+    Arm.Arm_serial_servo_write(1, angle, 1000)
 
 if __name__ == '__main__':
     while True:
