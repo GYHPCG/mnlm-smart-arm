d0c1a18e7ea442067873803980276fce97f2b910
破竹 <2986779260@qq.com>
2025-05-26 21:31:05 +0800

    将api key修改到.env文件中

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # mnlm-smart-arm
-code of LLM + arm 
+code of LLM + arm
+
+# 使用说明
+项目目前有3个分支，分别为main branch, v2 branch, v3 branch。
+其中main branch部分添加功能还在测试，不稳定。
+v2 branch具备基本功能，可以运行，可以正常使用。
+v3 branch是v2 branch的升级版本，目前功能已经稳定，可以正常使用。
+在使用v3 branch时候，你需要将.env.template文件重命名为.env，并填写你的OpenAI API Key,closeai API Key,QwenVla API Key。其中openai API Key和closeai API Key是可选的，如果你不需要视觉抓取功能的话。
+
+运行本项目，你需要进入client目录下的LLM_controller目录下，然后运行main.py
+然后你需要在你的机械臂端，即robot_arm文件下运行main.py,同理如果你需要机械臂视觉抓取共功能，你还需进入dofbot_ws/src下启动ROS服务端。启动命令如下：
+```shell
+cd ~/dofbot_ws/  # 进入工作空间
+catkin_make   # 编译
+source devel/setup.bash  # 更新系统环境
+roslaunch dofbot_info dofbot_server.launch  # 启动服务端节点
+```
+
--- robot_arm/utils_vlm.py
+++ robot_arm/utils_vlm.py
@@ -85,6 +85,7 @@ SYSTEM_PROMPT = '''
 
 # gpt4o调用函数
 import openai
+import os
 from openai import OpenAI
 import base64
 import json
@@ -106,7 +107,7 @@ def gpt4o_API(PROMPT='手上拿的东西放到旁边', img_path='../image/top_vi
     client = OpenAI(
         # openai系列的sdk，包括langchain，都需要这个/v1的后缀
         base_url='https://api.openai-proxy.org/v1',
-        api_key='sk-Cinx17W4V8Ss4B7HSfxUrf2kikhbvZE7EGHy5SYwWJBWs6Qm',
+        api_key=os.getenv('CLOSEAI_API_KEY'),
     )
     # 编码为base64数据
     base64_image = encode_image(img_path)
@@ -145,7 +146,7 @@ def QwenVL_API(PROMPT='手上拿的东西放到旁边', img_path='../image/top_v
     client = OpenAI(
         # openai系列的sdk，包括langchain，都需要这个/v1的后缀
         base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
-        api_key='sk-5976ceeed2fc4165b57d9b4d9c5d5f86',
+        api_key=os.getenv('CLOSEAI_API_KEY'),
     )
     # 编码为base64数据
     base64_image = encode_image(img_path)
@@ -212,7 +213,7 @@ def cv_gpt_get_xy(PROMPT='抓到到红色方块位置', img_path='../image/top_v
     client = OpenAI(
         # openai系列的sdk，包括langchain，都需要这个/v1的后缀
         base_url='https://api.openai-proxy.org/v1',
-        api_key='sk-Cinx17W4V8Ss4B7HSfxUrf2kikhbvZE7EGHy5SYwWJBWs6Qm',
+        api_key=os.getenv('CLOSEAI_API_KEY'),
     )
     # 编码为base64数据
     base64_image = encode_image(img_path)
@@ -255,7 +256,7 @@ def get_xy(PROMPT='移动到红色方块位置', img_path='../image/top_view_now
     client = OpenAI(
         # openai系列的sdk，包括langchain，都需要这个/v1的后缀
         base_url='https://api.openai-proxy.org/v1',
-        api_key='sk-Cinx17W4V8Ss4B7HSfxUrf2kikhbvZE7EGHy5SYwWJBWs6Qm',
+        api_key=os.getenv('CLOSEAI_API_KEY'),
     )
     # 编码为base64数据
     base64_image = encode_image(img_path)
